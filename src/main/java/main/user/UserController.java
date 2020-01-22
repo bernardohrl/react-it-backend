2d7de4ac27d355f6282a2d0e4a1ed6ac337f5a61
Bernardo Henrique <bernardohrl.unb@gmail.com>
2020-01-22 13:36:13 -0300

    Adds Post, Delete and Update User

--- src/main/java/main/user/UserController.java
+++ src/main/java/main/user/UserController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,14 +16,29 @@ public class UserController {
 	private UserService userService;
 	
 	
-	@RequestMapping("/users")
+	@RequestMapping(method=RequestMethod.GET, value="/users")
 	public List<User> getUsersList() {
 		return userService.getAllUsers();
 	}
 	
-	@RequestMapping("/users/{id}")
+	@RequestMapping(method=RequestMethod.GET, value="/users/{id}")
 	public User getUser(@PathVariable String id) {
 		return userService.getUser(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/users")
+	public void addUser(@RequestBody User user) {
+		userService.addUser(user); 
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT, value="/users/{id}")
+	public void updateUser(@RequestBody User user, @PathVariable String id) {
+		userService.updateUser(user, id);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE, value="/users/{id}")
+	public void deleteUser(@PathVariable String id) {
+		userService.deleteUser(id);
 	}
 	
 }
--- src/main/java/main/user/UserService.java
+++ src/main/java/main/user/UserService.java
@@ -1,5 +1,6 @@
 package main.user;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,17 +9,38 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-	private List<User> users = Arrays.asList(
+	private List<User> users = new ArrayList<User>(Arrays.asList(
 			new User("1", "Bernardo1", "CPF1"),
 			new User("2", "Bernardo2", "CPF2"),
 			new User("3", "Bernardo3", "CPF3")
-		);
+		));
+	
 	
 	public List<User> getAllUsers() {
 		return users;
 	}
 	
+	
 	public User getUser(String id) {
 		return users.stream().filter(user -> user.getId().equals(id)).findFirst().get();
+	}
+	
+	
+	public void addUser(User user) {
+		users.add(user);
+	}
+	
+	
+	public void updateUser(User user, String id) {
+		for(int i=0; i<users.size(); i++) {
+			if(users.get(i).getId().equals(id)) {
+				users.set(i, user);
+			}
+		}
+	}
+	
+	
+	public void deleteUser(String id) {
+		users.removeIf(user -> user.getId().equals(id));
 	}
 }
