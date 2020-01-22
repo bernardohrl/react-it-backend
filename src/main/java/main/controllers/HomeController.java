a06fcd001d2d5a02ddaef282bdef38291865fabf
Bernardo Henrique <bernardohrl.unb@gmail.com>
2020-01-22 11:49:13 -0300

    Initial Commit

---
+++ src/main/java/main/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package main.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String sayHello() {
+		return "hello!";
+	}
+	
+}
